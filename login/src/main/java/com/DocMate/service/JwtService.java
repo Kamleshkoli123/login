357876b5a832dbc203aa47e693ce2ecbfd8b4ac5
Kamleshkoli123 <kamleshkoli12799@gmail.com>
2024-12-22 21:05:12 +0530

    Update JwtService.java
    added admin access

--- login/src/main/java/com/DocMate/service/JwtService.java
+++ login/src/main/java/com/DocMate/service/JwtService.java
@@ -19,21 +19,38 @@ public class JwtService {
 
     @Value("${jwt.expiration:2592000000}") // Default 30 days in milliseconds
     private long jwtExpiration;
+    
+    @Value("${admin.contact}") 
+    private String adminContact;
+    
+    @Value("${admin.key}") 
+    private String adminKey;
+    
+    @Value("${admin.expiration}") // Default 30 days in milliseconds
+    private Long adminExpiry;
 
     // Generate JWT token
     public String generateJwtToken(String phoneNumber) {
+        logger.info("passkey: {}", secretKey);
+
+        // Determine role based on the phone number
+        String role = phoneNumber.equals(adminContact) ? adminKey : "user";
+
         try {
             return Jwts.builder()
-                .setSubject(phoneNumber)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS512)
-                .compact();
+                    .setSubject(phoneNumber)
+                    .claim("role", role) // Embed role in the token
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 
+                        (!role.equals("user") ? adminExpiry : jwtExpiration))) // 1 hour for admin, default for user
+                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS512)
+                    .compact();
         } catch (Exception e) {
             logger.error("Error generating JWT token for phone number: {}", phoneNumber, e);
             throw e;
         }
     }
+
 
     // Validate JWT token
     public void validateToken(String token) {
@@ -66,4 +83,19 @@ public class JwtService {
             throw new RuntimeException("Invalid JWT token.");
         }
     }
+    
+    public String extractRoleFromToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return claims.get("role", String.class);
+        } catch (Exception e) {
+            logger.error("Error extracting admin key from token: {}", token, e);
+            throw new RuntimeException("Invalid token");
+        }
+    }
+
 }
